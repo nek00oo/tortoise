e3115d1cc2ae53bd399bc0e306d9395d4b71c6c1
nek00oo <yakovenkovalera2017@yandex.ru>
2025-07-29 17:07:22 +0300

    change

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 def get_user_input():
-    name = input("Enter your username: ")
+    name = input("Enter your user_name: ")
     length = float(input("Enter rectangle length: "))
     width = float(input("Enter rectangle width: "))
     return name, length, width
@@ -9,5 +9,4 @@ def calculate_area(length, width):
 
 if __name__ == "__main__":
     user_name, user_length, user_width = get_user_input()
-    print("Area of rectangle:", calculate_area(6, 3))
     print("Area of rectangle:", calculate_area(user_length, user_width))
